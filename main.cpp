20047bedc767b71e4a1d9043ad96c8d1a2819343
Matthew-Hubbard <mrh8@pdx.edu>
2018-09-09 22:14:40 -0700

    ncurses menu testing...added -lmenu to g++ flags...

--- main.cpp
+++ main.cpp
@@ -1,16 +1,71 @@
 #include <iostream>
-#include <string>
+#include <stdlib.h>
+//#include <string>
 #include <ncurses.h>
+#include <menu.h>
+
+//#define ARRAY_SIZE 5
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#define CTRLD 4
 
 using namespace std;
 
+char *choices[] = {
+  "Choice 1",
+  "Choice 2",
+  "Choice 3",
+  "Choice 4",
+  "Exit",
+};
+
 int main()
 {
-  initscr(); //start curses mode
-  printw("Hello World!!!"); //print
-  refresh(); //print it on the real screen
-  getch(); //wait for user input
-  endwin(); //end curses mode
+  ITEM ** my_items;
+  int c;        
+  MENU * my_menu;
+  int n_choices, i;
+  ITEM * cur_item;
+
+
+  initscr();
+  cbreak();
+  noecho();
+  keypad(stdscr, TRUE);
+
+  my_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
+
+  for(i = 0; i < n_choices; ++i)
+    my_items[i] = new_item(choices[i], choices[i]);
+  my_items[n_choices] = (ITEM *)NULL;
+
+  my_menu = new_menu((ITEM **)my_items);
+  mvprintw(LINES - 2, 0, "<- to Exit");
+  post_menu(my_menu);
+  refresh();
+
+  while((c = getch()) != KEY_LEFT)
+  {   switch(c)
+    { case KEY_DOWN:
+      menu_driver(my_menu, REQ_DOWN_ITEM);
+      break;
+      case KEY_UP:
+      menu_driver(my_menu, REQ_UP_ITEM);
+      break;
+    }
+  } 
+
+  free_item(my_items[0]);
+  free_item(my_items[1]);
+  free_menu(my_menu);
+  endwin();
+
+  /*
+     initscr(); //start curses mode
+     printw("Hello World!!!"); //print
+     refresh(); //print it on the real screen
+     getch(); //wait for user input
+     endwin(); //end curses mode
+     */
 
   return 0;
 }
--- makefile
+++ makefile
@@ -1,2 +1,5 @@
 all:
-	g++ -g -Wall -o run main.cpp -lncurses
+	g++ -g -Wall -o run main.cpp -lmenu -lncurses
+test:
+	gcc -g -Wall -o test menu_test.c -lmenu -lncurses
+
